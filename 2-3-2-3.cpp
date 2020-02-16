128cabac2e42aa165f4b4a54803aea59e0efa136
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-02-16 20:28:39 +0800

    MultiPatternSum

--- 2-3-2-3.cpp
+++ 2-3-2-3.cpp
@@ -9,22 +9,44 @@ int n = 0;
 int a[MAX_N], m[MAX_N];
 int K = 0;
 
-bool dp[MAX_N][MAX_K+1];
+bool dp1[MAX_N][MAX_K+1];
+int dp2[MAX_N][MAX_K+1];
 
 bool solve1() {
   for (int j = 0; j <= K; ++j) {
-    if (j % a[0] == 0 && j / a[0] <= m[0]) dp[0][j] = true;
+    if (j % a[0] == 0 && j / a[0] <= m[0]) dp1[0][j] = true;
   }
   for (int i = 1; i < n; ++i) {
     for (int j = 0; j <= K; ++j) {
       for (int k = 0; k <= m[i] && k * a[i] <= j; ++k) {
-        dp[i][j] = dp[i][j] || dp[i-1][j-k*a[i]];
+        dp1[i][j] = dp1[i][j] || dp1[i-1][j-k*a[i]];
       }
     }
   }
-  return dp[n-1][K];
+  return dp1[n-1][K];
 }
 
+bool solve2() {
+  for (int j = 0; j <= K; ++j) {
+    if (j % a[0] == 0 && j / a[0] <= m[0]) dp2[0][j] = m[0] - j / a[0];
+  }
+  for (int i = 1; i < n; ++i) {
+    for (int j = 0; j <= K; ++j) {
+      if (dp2[i-1][j] >= 0) {
+        dp2[i][j] = m[i];
+      }
+      else {
+        if (j >= a[i] && dp2[i][j-a[i]] >= 1) {
+          dp2[i][j] = dp2[i][j-a[i]] - 1;
+        }
+        else {
+          dp2[i][j] = -1;
+        }
+      }
+    }
+  }
+  return dp2[n-1][K] >= 0;
+}
 
 int main() {
   cin >> n;
@@ -35,10 +57,16 @@ int main() {
   cin >> K;
   for (int i = 0; i < MAX_N; ++i) {
     for (int j = 0; j <= MAX_K; ++j) {
-      dp[i][j] = false;
+      dp1[i][j] = false;
     }
   }
-  bool ans = solve1();
+  for (int i = 0; i < MAX_N; ++i) {
+    for (int j = 0; j <= MAX_K; ++j) {
+      dp2[i][j] = -1;
+    }
+  }
+  // bool ans = solve1();
+  bool ans = solve2();
   if (ans) {
     cout << "Yes" << endl;
   }
