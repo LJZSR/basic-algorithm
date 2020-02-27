5ec35b0bc9bd4460240d14c79c05ac9f1a87f4c2
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-02-27 21:21:39 +0800

    DoubleSix

--- 2-6-1-2.cpp
+++ 2-6-1-2.cpp
@@ -2,8 +2,32 @@
 
 using namespace std;
 
+int a = 0, b = 0;
+int x = 0, y = 0;
+
+int exgcd(int a, int b, int &x, int &y) {
+  int ans = a;
+  if (b == 0) {
+    x = 1;
+    y = 0;
+  }
+  else {
+    ans = exgcd(b, a%b, x, y);
+    int tmp = y;
+    y = x - (a/b)*y;
+    x = tmp;
+  }
+  return ans;
+}
+
 int main() {
-
-
+  cin >> a >> b;
+  int ans = exgcd(a, b, x, y);
+  if (ans == 1) {
+    cout << x << " " << y << endl;
+  }
+  else {
+    cout << -1 << endl;
+  }
   return 0;
 }
