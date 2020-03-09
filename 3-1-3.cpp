011a6a478725ba4a821a453091c3e6fde8fa9e2c
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-03-09 11:16:04 +0800

    Aggressive cows

---
+++ 3-1-3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int MAX_N = 100000;
+
+int X[MAX_N];
+int N, M;
+
+bool judge(int c) {
+  int gap = X[c] - X[0];
+  int curP = X[c];
+  int num = 2;
+  for (int i = c+1; i < N; ++i) {
+    if (X[i] - curP >= gap) {
+      ++num;
+      curP = X[i];
+    }
+  }
+  return num >= M;
+}
+
+int solve() {
+  sort(X, X + N);
+  int lb = -1, ub = N;
+  while (ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (judge(mid)) {
+      lb = mid - 1;
+    }
+    else {
+      ub = mid - 1;
+    }
+  }
+  return X[ub] - X[0];
+}
+
+int main() {
+  cin >> N >> M;
+  for (int i = 0; i < N; ++i) {
+    cin >> X[i];
+  }
+
+  int ans = solve();
+  cout << ans << endl;
+  return 0;
+}
