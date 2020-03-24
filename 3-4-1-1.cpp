b6fa7f96df2e8d52d8b39c2e0d53552eabb5e5fd
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-03-24 13:06:11 +0800

    TSP

--- 3-4-1-1.cpp
+++ 3-4-1-1.cpp
@@ -1,4 +1,52 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+const int INF = 16000;
+const int MAX_N = 15;
+
+int N, E;
+int d[MAX_N][MAX_N];
+
+int dp[1 << MAX_N][MAX_N];
+
+int res(int S, int v) {
+  if (dp[S][v] > 0) return dp[S][v];
+  if (S == (1 << N) - 1 && v == 0) {
+    dp[S][v] = 0;
+    return 0;
+  }
+  int ans = INF;
+  for (int i = 0; i < N; ++i) {
+    if (((S >> i) & 1) != 1) {
+      ans = min(ans, res((S | (1 << i)), i) + d[v][i]);
+    }
+  }
+  dp[S][v] = ans;
+  return ans;
+}
+
+void solve() {
+  memset(dp, -1, sizeof(dp));
+  int ans = res(0, 0);
+  cout << ans << endl;
+}
+
+int main() {
+  cin >> N >> E;
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      d[i][j] = INF;
+    }
+  }
+  for (int i = 0; i < E; ++i) {
+    int begin;
+    int end;
+    int cost;
+    cin >> begin >> end >> cost;
+    d[begin][end] = cost;
+  }
+  solve();
+  return 0;
+}
